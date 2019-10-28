37c9aa8413a4bfcf7f70b2ec1b10ce7594d1a87a
jintaozhao <57077308+jintaozhao@users.noreply.github.com>
2019-10-28 11:14:38 +0800

    Update domo.cs

--- domo.cs
+++ domo.cs
@@ -1,1 +1,12 @@
-Console.WriteLine("  Hello  ");
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I love programing"); //output text
+            Console.WriteLine("I must study hard"); //output text
+            Console.ReadKey();// Fixed console interface
+        }
+    }
+}
